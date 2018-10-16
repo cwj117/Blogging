60b8603f5cd7702b3e16cabe6b39ae401fff0eff
Kiran Jasvanee <kiran.jasvanee@yahoo.com>
2018-10-16 13:53:45 +0530

    Uploading Unowned Vs Weak self in closure first commit

---
+++ UnownedVsWeak/UnownedVsWeak/UnownedVsWeakViewController.swift
@@ -0,0 +1,76 @@
+//
+//  UnownedVsWeakViewController.swift
+//  UnownedVsWeak
+//
+//  Created by MAC241 on 10/16/18.
+//  Copyright © 2018 MAC241. All rights reserved.
+//
+
+import UIKit
+
+enum ReferenceChoice {
+    case unowned
+    case weak
+    case none
+}
+
+class UnownedVsWeakViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var referenceChoice: ReferenceChoice = .none
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // curve the imageview.
+        self.imageView.layer.cornerRadius = 8.0
+        self.imageView.clipsToBounds = true
+        
+        switch self.referenceChoice {
+        case .unowned:
+            self.tryUnowned()
+        case .weak:
+            self.tryWeak()
+        case .none:
+            print("No action to be taken")
+        }
+    }
+    
+    func tryUnowned() {
+        // let's get image data from server to load it in our imageview.
+        self.getData(from: URL.init(string: "https://images.pexels.com/photos/221433/pexels-photo-221433.jpeg")!) { [unowned self] (data, response, error) in
+            DispatchQueue.main.async {
+                self.imageView.image = UIImage.init(data: data!)
+            }
+        }
+    }
+    func tryWeak() {
+        // let's get image data from server to load it in our imageview.
+        self.getData(from: URL.init(string: "https://images.pexels.com/photos/221433/pexels-photo-221433.jpeg")!) { [weak self] (data, response, error) in
+            DispatchQueue.main.async {
+                self?.imageView.image = UIImage.init(data: data!)
+            }
+        }
+    }
+    
+    func getData(from url: URL, completion: @escaping (Data?, URLResponse?, Error?) -> ()) {
+        URLSession.shared.dataTask(with: url, completionHandler: completion).resume()
+    }
+    
+    deinit {
+        print("Checking Unowned Controller deallocated")
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ UnownedVsWeak/UnownedVsWeak/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  UnownedVsWeak
+//
+//  Created by Kiran Jasvanee on 10/15/18.
+//  Copyright © 2018 Kiran Jasvanee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
